82684dd8f5247428f67ccaa3bbba1c3344e16d40
LijuJacob08 <liju.jaocb08@gmail.com>
2021-12-17 19:35:39 +0530

    Intro to Hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Songlist = () => {
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>this wild darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Songlist;
